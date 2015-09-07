09aca48b858afb20845c21bf7e25faf2ad611cc7
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2015-09-07 21:17:48 +0200

    Towards cats.data.Validated

--- validation-core/src/main/scala/play/api/data/mapping/package.scala
+++ validation-core/src/main/scala/play/api/data/mapping/package.scala
@@ -1,6 +1,7 @@
 package jto
 
-import cats.{Apply, Unapply}
+import cats.{Apply, Monoid, Unapply}
+import cats.data.Validated
 import cats.syntax.{ApplyOps, ApplySyntax1}
 
 /**
@@ -16,12 +17,16 @@ import cats.syntax.{ApplyOps, ApplySyntax1}
  */
 package object validation {
   @annotation.implicitNotFound("No implicit Mapping found from ${I} to ${O}. Try to define an implicit Mapping[${E}, ${I}, ${O}].")
-  type Mapping[E, I, O] = I => Validation[E, O]
+  type Mapping[E, I, O] = I => Validated[Seq[E], O]
   type Constraint[T] = Mapping[ValidationError, T, T]
-  type VA[O] = Validation[(Path, Seq[ValidationError]), O]
+  type VA[O] = Validated[Seq[(Path, Seq[ValidationError])], O]
   
-  @deprecated("unlift can now be omitted", "2.0")
-  def unlift[A, B](f: A => Option[B]): A => B = Function.unlift(f)
+  type Validated[+E, +A] = cats.data.Validated[E, A]
+  val Validated = cats.data.Validated
+  type Valid[+A] = cats.data.Validated.Valid[A]
+  val Valid = cats.data.Validated.Valid
+  type Invalid[+E] = cats.data.Validated.Invalid[E]
+  val Invalid = cats.data.Validated.Invalid
   
   implicit def toFunctionalBuilderOps[M[_], A](a: M[A])(implicit fcb: FunctionalCanBuild[M]): FunctionalBuilderOps[M, A] = new FunctionalBuilderOps[M, A](a)(fcb)
   
@@ -29,4 +34,10 @@ package object validation {
     object As extends ApplySyntax1
     As.applySyntaxU(fa)
   }
+  
+  implicit def seqAlgebra[A]: Monoid[Seq[A]] =
+    new Monoid[Seq[A]] {
+      def empty: Seq[A] = Seq.empty[A]
+      def combine(x: Seq[A], y: Seq[A]): Seq[A] = x ++ y
+    }
 }

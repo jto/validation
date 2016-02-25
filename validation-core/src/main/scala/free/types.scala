75392b06118c42d36e5f5912ea681998a6573977
jto <boudhevil@gmail.com>
2016-02-25 11:50:16 +0100

    Working type matching

--- validation-core/src/main/scala/free/types.scala
+++ validation-core/src/main/scala/free/types.scala
@@ -35,14 +35,12 @@ object Outer {
     }
 }
 
-
-
 trait Destruct[FA] {
   type F[_]
   type A
 }
 
-trait LowPriorityDestruct {
+object Destruct {
   type Aux[FA0, F0[_], A0] =
     Destruct[FA0] {
       type F[α] = F0[α]
@@ -54,100 +52,40 @@ trait LowPriorityDestruct {
       type F[α] = out.F[α]
       type A = in.A
     }
-}
 
-object Destruct extends LowPriorityDestruct {
   def apply[FA](implicit d: Destruct[FA]): Aux[FA, d.F, d.A] = d
-
-  // implicit def recDestruct[FA, A](implicit out: Outer[FA], in: Inner.Aux[FA, A], d: Destruct[A]): Aux[FA, λ[α => out.F[d.F[α]]], d.A] =
-  //   new Destruct[FA] {
-  //     type F[α] = out.F[d.F[α]]
-  //     type A = d.A
-  //   }
 }
 
-trait Inner1[FA[_]] {
-  type A[_]
+trait Match[F[_], FA] {
+  type A
 }
 
-object Inner1 {
-  type Aux[FA[_], A0[_]] = Inner1[FA] {
-    type A[α] = A0[α]
-  }
+trait LowPriorityMatch {
+  type Aux[F0[_], FA, A0] = Match[F0, FA] { type A = A0 }
 
-  def apply[FA[_]](implicit is: Inner1[FA]): Aux[FA, is.A] = is
-
-  implicit def inner1[F0[_], A0[_]]: Aux[λ[α => F0[A0[α]]], A0] =
-    new Inner1[λ[α => F0[A0[α]]]] {
-      type A[α] = A0[α]
+  implicit def defaultMatch[F0[_], FA](implicit d: Destruct[FA] { type F[α] = F0[α] }): Aux[F0, FA, d.A] =
+    new Match[F0, FA] {
+      type A = d.A
     }
 }
 
-trait Outer1[FA[_]] {
-  type F[_]
+object Match extends LowPriorityMatch {
+
+  implicit def recMatch[Out[_], F[_], FA, A0](implicit
+    dfa: Destruct.Aux[FA, Out, A0],
+    m: Match[F, A0]
+  ): Aux[λ[α => Out[F[α]]], FA, m.A] = new Match[λ[α => Out[F[α]]], FA] { type A = m.A }
+
+  def apply[F[_], FA](implicit m: Match[F, FA]): Aux[F, FA, m.A] = m
 }
 
-object Outer1 {
-  type Aux[FA[_], F0[_]] = Outer1[FA] {
-    type F[α] = F0[α]
-  }
+object test {
+  Match[Option, Option[Int]]
+  Match[List, List[Option[Int]]]
 
-  def apply[FA[_]](implicit is: Outer1[FA]): Aux[FA, is.F] = is
+  type Foo[α] = List[Option[α]]
+  Match[Foo, List[Option[Int]]]
 
-  implicit def outer1[F0[_], A0[_]]: Aux[λ[α => F0[A0[α]]], F0] =
-    new Outer1[λ[α => F0[A0[α]]]] {
-      type F[α] = F0[α]
-    }
+  Match[λ[α => List[Option[α]]], List[Option[Int]]]
 }
-
-
-trait Destruct1[FA[_]] {
-  type F[_]
-  type A[_]
-}
-
-trait LowPriorityDestruct1 {
-  type Aux[FA0[_], F0[_], A0[_]] =
-    Destruct1[FA0] {
-      type F[X] = F0[X]
-      type A[X] = A0[X]
-    }
-
-  implicit def defaultDestruct1[FA[_]](implicit out: Outer1[FA], in: Inner1[FA]): Aux[FA, out.F, in.A] =
-    new Destruct1[FA] {
-      type F[α] = out.F[α]
-      type A[α] = in.A[α]
-    }
-}
-
-object Destruct1 extends LowPriorityDestruct1 {
-  def apply[FA[_]](implicit d: Destruct1[FA]): Aux[FA, d.F, d.A] = d
-
-  // implicit def recDestruct[FA[_], A[_]](implicit out: Outer1[FA], in: Inner1.Aux[FA, A], d: Destruct1[A]): Aux[FA, λ[α => out.F[d.F[α]]], d.A] =
-  //   new Destruct1[FA] {
-  //     type F[α] = out.F[d.F[α]]
-  //     type A[α] = d.A[α]
-  //   }
-}
-
-// trait Match[F[_], FA] {
-//   type A
-// }
-
-// trait LowPriorityMatch {
-//   type Aux[F0[_], FA, A0] = Match[F0, FA] { type A = A0 }
-
-//   implicit def defaultMatch[F[_], FA](implicit out: Outer.Aux[FA, F], in: Inner[FA]): Aux[F, FA, in.A] =
-//     new Match[F, FA] {
-//       type A = in.A
-//     }
-// }
-
-// object Match {
-//   implicit def recMatch[F[_], FA, Out[_], F1[_], A](implicit
-//     df: Destruct1.Aux[F, Out, F1],
-//     dfa: Destruct.Aux[FA, Out, A],
-//     m: Match[F1, A]
-//   ) = ???
-// }
 

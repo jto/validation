92c3e628a249cbd3edb6390f70e595ba2cb21d52
jto <boudhevil@gmail.com>
2016-02-25 21:48:54 +0100

    more matchers

--- validation-core/src/main/scala/free/types.scala
+++ validation-core/src/main/scala/free/types.scala
@@ -36,16 +36,34 @@ object Outer {
 }
 
 trait Match[Fτ, FA] {
+  type F[_]
   type A
+  def cast[G[_]](fa: FA): G[A] = fa.asInstanceOf[G[A]]
 }
 
 trait LowPriorityMatch {
-  type Aux[Fτ, FA, A0] = Match[Fτ, FA] { type A = A0 }
+  type Aux[Fτ, FA, F0[_], A0] = Match[Fτ, FA] {
+    type F[α] = F0[α]
+    type A = A0
+  }
 
   sealed trait τ
 
-  implicit def match0[F[_], A0]: Aux[F[τ], F[A0], A0] =
-    new Match[F[τ], F[A0]] {
+  implicit def match0[F0[_], A0]: Aux[F0[τ], F0[A0], F0, A0] =
+    new Match[F0[τ], F0[A0]] {
+      type F[α] = F0[α]
+      type A = A0
+    }
+
+  implicit def match0HK1[F0[_, _], A0, A1]: Aux[F0[τ, A1], F0[A0, A1], F0[?, A1], A0] =
+    new Match[F0[τ, A1], F0[A0, A1]] {
+      type F[α] = F0[α, A1]
+      type A = A0
+    }
+
+  implicit def match0HK2[F0[_, _], A0, A1]: Aux[F0[A1, τ], F0[A1, A0], F0[A1, ?], A0] =
+    new Match[F0[A1, τ], F0[A1, A0]] {
+      type F[α] = F0[A1, α]
       type A = A0
     }
 }
@@ -59,9 +77,12 @@ object Match extends LowPriorityMatch {
       inτ: Inner.Aux[Fτ, Aτ],
       infa: Inner.Aux[FA, AFA],
       m: Match[Aτ, AFA]
-    ): Aux[Fτ, FA, m.A] = new Match[Fτ, FA] { type A = m.A }
+    ): Aux[Fτ, FA, λ[α => Out[m.F[α]]], m.A] = new Match[Fτ, FA] {
+      type F[α] = Out[m.F[α]]
+      type A = m.A
+    }
 
-  def apply[Fτ, FA](implicit m: Match[Fτ, FA]): Aux[Fτ, FA, m.A] = m
+  def apply[Fτ, FA](implicit m: Match[Fτ, FA]): Aux[Fτ, FA, m.F, m.A] = m
 }
 
 object test {
@@ -74,12 +95,22 @@ object test {
   Match[Foo[τ], Foo[Int]]   // compiles
   // Match[Foo[τ], List[Option[Int]]]   // should compile ? does not.
 
-  Match[List[Option[τ]], List[Option[Int]]] // compiles
+  val m1 = Match[List[Option[τ]], List[Option[Int]]] // compiles
+  val m2 = Match[List[Option[τ]], List[Option[Int]]] // compiles
+
   Match[List[Option[τ]], List[Option[List[Int]]]] // compiles
   Match[List[Option[List[τ]]], List[Option[List[Int]]]] // compile
 
-  Match[Option[τ], List[Option[List[Int]]]] // should not compile
+  Match[Either[τ, Int], Either[String, Int]] // compile
+  Match[Either[String, τ], Either[String, Int]] // compile
+  Match[List[Either[String, τ]], List[Either[String, Int]]] // compile
+  Match[Option[List[Either[String, τ]]], Option[List[Either[String, Int]]]] // compile
+
+  // Match[Either[Int, τ], Either[String, Int]] // does not compile
+
+  // Match[Option[τ], List[Option[List[Int]]]] // should not compile
   // Match[List[τ], Option[List[Int]]] // should not compile
   // Match[List[List[τ]], List[Option[List[Int]]]] // should not compile
+  // Match[List[List[List[τ]]], List[Option[List[Int]]]] // should not compile
 }
 

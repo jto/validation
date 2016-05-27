7e89046dfb5875b8a4c657ba7c6f55a6d9074b2d
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2016-05-27 12:02:14 +0200

    Update example to return { isSuccess/output/errors }

--- play-scalajs-example/js/src/main/scala/Validate.scala
+++ play-scalajs-example/js/src/main/scala/Validate.scala
@@ -1,22 +1,26 @@
 package client
 
-import jto.validation.{Rule, Valid, Invalid, VA}
+import jto.validation.{Format, Valid, Invalid, VA}
 import jto.validation.jsjson.{Rules, Writes}
 import scala.scalajs.js
 import js.annotation.JSExport
 import model.User
+import scala.Function.const
 
 @JSExport
 object Validate {
   @JSExport
-  def user(json: js.Dynamic): js.Dynamic = {
+  def user(json: js.Dynamic): js.Dictionary[Any] = {
     import Rules._, Writes._
 
-    val rule: Rule[js.Dynamic, User] = Rule.gen
-    val validated: VA[User] = rule.validate(json)
-    validated match {
-      case e @ Invalid(_) => failureW.writes(e)
-      case Valid(_) => json
-    }
+    val format = Format.gen[js.Dynamic, js.Dynamic, User]
+
+    val validated: VA[User] = format.validate(json)
+
+    js.Dictionary(
+      "isSuccess" -> validated.fold(const(false), const(true)),
+      "output"    -> validated.fold(const(null), format.writes),
+      "errors"    -> validated.fold(e => failureW.writes(Invalid(e)), const(null))
+    )
   }
 }

e46f04b3491b9840fd8d131073e70d07a8455f84
Olivier Blanvillain <olivier.blanvillain@gmail.com>
2016-06-23 16:26:11 +0200

    /misc → /scripts

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,7 +9,7 @@ coverage="$sbt_cmd clean coverage validationJVM/test coverageReport && sbt cover
 
 compile_example="$sbt_cmd publish-local && (cd play-scalajs-example && $sbt_cmd compile)"
 
-compile_doc="bash misc/build-book.sh"
+compile_doc="bash scripts/build-book.sh"
 
 run_cmd="$coverage && $test_cmd && $compile_example && $compile_doc"
 

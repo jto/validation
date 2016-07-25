50df510daec36b4ce1f0aa6cbc024e77f2d5c7f9
Antoine Allioux <antoine.allioux@mfglabs.com>
2016-07-25 14:58:16 +0200

    Downgraded Play dependecy to version 2.3.9

--- validation-jsonast/jvm/src/main/scala/Ast.scala
+++ validation-jsonast/jvm/src/main/scala/Ast.scala
@@ -6,20 +6,22 @@ import play.api.libs.json._
 object Ast {
   val to: Write[JValue, JsValue] = Write[JValue, JsValue] {
     case JNull           => JsNull
-    case JObject (value) => JsObject(value.mapValues(to.writes))
+    case JObject (value) => JsObject(value.mapValues(to.writes).toSeq)
     case JArray  (value) => JsArray(value.map(to.writes))
     case JBoolean(value) => JsBoolean(value)
     case JString (value) => JsString(value)
     case JNumber (value) => JsNumber(BigDecimal(value))
+    case JUndefined(value) => JsUndefined(value)
   }
 
   private def totalFrom(jsValue: JsValue): JValue = jsValue match {
     case JsNull           => JNull
-    case JsObject (value) => JObject(value.mapValues(totalFrom).toMap)
+    case JsObject (value) => JObject(value.toMap.mapValues(totalFrom))
     case JsArray  (value) => JArray(value.map(totalFrom).toVector)
     case JsBoolean(value) => JBoolean(value)
     case JsString (value) => JString(value)
     case JsNumber (value) => JNumber(value.toString)
+    case undef : JsUndefined => JUndefined(undef.error)
   }
 
   val from: Rule[JsValue, JValue] = Rule(x => Valid(totalFrom(x)))

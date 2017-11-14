39583cfa59442a386ffabe95949ffc6297c2abfa
jto <boudhevil@gmail.com>
2017-11-14 21:28:55 +0100

    cleanup

--- validation-playjson/src/test/scala/tagless/JsonTestCases.scala
+++ validation-playjson/src/test/scala/tagless/JsonTestCases.scala
@@ -2,122 +2,121 @@ package jto.validation
 package v3.tagless
 package playjson
 
-import play.api.libs.json._
+import play.api.libs.json._, Json.obj
 
 object JsonTestCases extends TestCases[JsValue] {
 
   override val base = new base {
 
-    val id = Json.obj("id" -> "1")
+    val id = obj("id" -> "1")
 
     val info =
-      Json.obj("label" -> "Personal",
-               "email" -> "fakecontact@gmail.com",
-               "phones" -> Seq("01.23.45.67.89", "98.76.54.32.10"))
+      obj("label" -> "Personal",
+          "email" -> "fakecontact@gmail.com",
+          "phones" -> Seq("01.23.45.67.89", "98.76.54.32.10"))
 
     val infoNoLabel =
-      Json.obj("email" -> "fakecontact@gmail.com",
-               "phones" -> Seq("01.23.45.67.89", "98.76.54.32.10"))
+      obj("email" -> "fakecontact@gmail.com",
+          "phones" -> Seq("01.23.45.67.89", "98.76.54.32.10"))
 
-    val noInfo = Json.obj()
+    val noInfo = obj()
 
-    val jto = Json.obj(
-      "firstname" -> "Julien",
-      "lastname" -> "Tournay",
-      "informations" -> Seq((Json.obj("label" -> "Personal") ++ info)))
+    val jto = obj("firstname" -> "Julien",
+                  "lastname" -> "Tournay",
+                  "informations" -> Seq((obj("label" -> "Personal") ++ info)))
 
     val valid =
-      Json.obj("firstname" -> "Julien",
-               "lastname" -> "Tournay",
-               "age" -> 27,
-               "informations" -> info,
-               "contacts" -> Seq(info))
+      obj("firstname" -> "Julien",
+          "lastname" -> "Tournay",
+          "age" -> 27,
+          "informations" -> info,
+          "contacts" -> Seq(info))
 
     val invalid =
-      Json.obj(
+      obj(
         "firstname" -> "Julien",
         "lastname" -> "Tournay",
         "age" -> 27,
         "informations" ->
-          (Json.obj("label" -> "") ++ infoNoLabel),
-        "contacts" -> Seq(Json.obj("label" -> "") ++ infoNoLabel)
+          (obj("label" -> "") ++ infoNoLabel),
+        "contacts" -> Seq(obj("label" -> "") ++ infoNoLabel)
       )
 
-    val smthTrue = Json.obj("issmth" -> true)
-    val smthFalse = Json.obj("issmth" -> false)
-    val emptyObj = Json.obj()
+    val smthTrue = obj("issmth" -> true)
+    val smthFalse = obj("issmth" -> false)
+    val emptyObj = obj()
   }
 
   val int = new int {
-    val ok = Json.obj("n" -> 4)
-    val foo = Json.obj("n" -> "foo")
-    val float = Json.obj("n" -> 4.5)
-    val bigdecimal = Json.obj("n" -> 4.5)
-    val noOK = Json.obj("n" -> Json.obj("o" -> 4))
-    val noFoo = Json.obj("n" -> Json.obj("o" -> "foo"))
-    val nopOK = Json.obj("n" -> Json.obj("o" -> Json.obj("p" -> 4)))
-    val nopFoo = Json.obj("n" -> Json.obj("o" -> Json.obj("p" -> "foo")))
+    val ok = obj("n" -> 4)
+    val foo = obj("n" -> "foo")
+    val float = obj("n" -> 4.5)
+    val bigdecimal = obj("n" -> 4.5)
+    val noOK = obj("n" -> obj("o" -> 4))
+    val noFoo = obj("n" -> obj("o" -> "foo"))
+    val nopOK = obj("n" -> obj("o" -> obj("p" -> 4)))
+    val nopFoo = obj("n" -> obj("o" -> obj("p" -> "foo")))
   }
 
   val boolean = new boolean {
-    val ok = Json.obj("n" -> true)
+    val ok = obj("n" -> true)
     val foo = int.foo
   }
 
   val string = new string {
     val foo = int.foo
-    val foos = Json.obj("n" -> Seq("foo"))
-    val _42 = Json.obj("n" -> 42)
-    val onFoo = Json.obj("o" -> Json.obj("n" -> "foo"))
+    val foos = obj("n" -> Seq("foo"))
+    val _42 = obj("n" -> 42)
+    val onFoo = obj("o" -> obj("n" -> "foo"))
   }
 
   val option = new option {
-    val nNull = Json.obj("n" -> JsNull)
-    val fooBar = Json.obj("foo" -> "bar")
-    val nBar = Json.obj("n" -> "bar")
+    val nNull = obj("n" -> JsNull)
+    val fooBar = obj("foo" -> "bar")
+    val nBar = obj("n" -> "bar")
     val none = JsObject(Nil)
   }
 
   val seq = new seq {
-    val foos = Json.obj("n" -> Seq("foo"))
-    val fooBars = Json.obj("foo" -> Seq("bar"))
-    val foofoobars = Json.obj("foo" -> Json.obj("foo" -> Seq("bar")))
-    val ns = Json.obj("n" -> Seq("foo", ""))
-    val ints = Json.obj("n" -> Seq(1, 2, 3))
-    val paf = Json.obj("n" -> "paf")
+    val foos = obj("n" -> Seq("foo"))
+    val fooBars = obj("foo" -> Seq("bar"))
+    val foofoobars = obj("foo" -> obj("foo" -> Seq("bar")))
+    val ns = obj("n" -> Seq("foo", ""))
+    val ints = obj("n" -> Seq(1, 2, 3))
+    val paf = obj("n" -> "paf")
     val mixed = JsObject(Seq("n" -> JsArray(Seq(JsString("foo"), JsNumber(2)))))
   }
 
   val map = new map {
-    val foobar = Json.obj("n" -> Json.obj("foo" -> List("bar")))
-    val ints = Json.obj("n" -> Json.obj("foo" -> List(4), "bar" -> List(5)))
+    val foobar = obj("n" -> obj("foo" -> List("bar")))
+    val ints = obj("n" -> obj("foo" -> List(4), "bar" -> List(5)))
     val mixed =
-      Json.obj("n" -> Json.obj("foo" -> List(4), "bar" -> List("frack")))
+      obj("n" -> obj("foo" -> List(4), "bar" -> List("frack")))
   }
 
   val password = new password {
     val ok =
-      Json.obj("login" -> "Alice", "password" -> "s3cr3t", "verify" -> "s3cr3t")
+      obj("login" -> "Alice", "password" -> "s3cr3t", "verify" -> "s3cr3t")
 
     val empty =
-      Json.obj("login" -> "Alice", "password" -> "s3cr3t", "verify" -> "")
+      obj("login" -> "Alice", "password" -> "s3cr3t", "verify" -> "")
 
     val err =
-      Json.obj("login" -> "Alice", "password" -> "s3cr3t", "verify" -> "bam")
+      obj("login" -> "Alice", "password" -> "s3cr3t", "verify" -> "bam")
   }
 
   val subclasses = new subclasses {
-    val b = Json.obj("name" -> "B", "foo" -> 4)
-    val c = Json.obj("name" -> "C", "bar" -> 6)
-    val e = Json.obj("name" -> "E", "eee" -> 6)
+    val b = obj("name" -> "B", "foo" -> 4)
+    val c = obj("name" -> "C", "bar" -> 6)
+    val e = obj("name" -> "E", "eee" -> 6)
   }
 
   val rec = new rec {
     val bobAndFriends =
-      Json.obj("name" -> "bob", "friends" -> Seq(Json.obj("name" -> "tom")))
+      obj("name" -> "bob", "friends" -> Seq(obj("name" -> "tom")))
 
     val bobAndFriend =
-      Json.obj("name" -> "bob", "friend" -> Json.obj("name" -> "tom"))
+      obj("name" -> "bob", "friend" -> obj("name" -> "tom"))
   }
 
 }
